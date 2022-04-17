0eeb835d4b2f769e42ca486afcbd266b71bbbc48
danielenapo <gnele2000@gmail.com>
2022-04-17 22:22:19 +0200

    client GET CoAP

---
+++ app/src/main/java/com/example/coapplugin/GETClient.java
@@ -0,0 +1,95 @@
+package com.example.coapplugin;
+/*******************************************************************************
+ * Copyright (c) 2015 Institute for Pervasive Computing, ETH Zurich and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ *
+ * Contributors:
+ *    Matthias Kovatsch - creator and main architect
+ *    Achim Kraus (Bosch Software Innovations GmbH) - add saving payload
+ ******************************************************************************/
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.Utils;
+import org.eclipse.californium.core.config.CoapConfig;
+import org.eclipse.californium.elements.config.Configuration;
+import org.eclipse.californium.elements.config.Configuration.DefinitionsProvider;
+import org.eclipse.californium.elements.config.UdpConfig;
+import org.eclipse.californium.elements.exception.ConnectorException;
+
+
+public class GETClient {
+
+    private static final File CONFIG_FILE = new File("Californium3.properties");
+    private static final String CONFIG_HEADER = "Californium CoAP Properties file for client";
+    private static final int DEFAULT_MAX_RESOURCE_SIZE = 2 * 1024 * 1024; // 2 MB
+    private static final int DEFAULT_BLOCK_SIZE = 512;
+    private String response;
+
+    static {
+        CoapConfig.register();
+        UdpConfig.register();
+    }
+
+    private static DefinitionsProvider DEFAULTS = new DefinitionsProvider() {
+
+        @Override
+        public void applyDefinitions(Configuration config) {
+            config.set(CoapConfig.MAX_RESOURCE_BODY_SIZE, DEFAULT_MAX_RESOURCE_SIZE);
+            config.set(CoapConfig.MAX_MESSAGE_SIZE, DEFAULT_BLOCK_SIZE);
+            config.set(CoapConfig.PREFERRED_BLOCK_SIZE, DEFAULT_BLOCK_SIZE);
+        }
+    };
+
+    /*
+     * Application entry point.
+     */
+    public String getResponse(String ip, String resource) {
+        Configuration config = Configuration.createWithFile(CONFIG_FILE, CONFIG_HEADER, DEFAULTS);
+        Configuration.setStandard(config);
+
+        URI uri = null; // URI parameter of the request
+
+
+        // input URI from command line arguments
+        try {
+            uri = new URI("coap://" + ip + ":5683/" + resource);
+        } catch (URISyntaxException e) {
+            System.err.println("Invalid URI: " + e.getMessage());
+            System.exit(-1);
+            response= "invalid URI" + e.getMessage();
+        }
+
+        CoapClient client = new CoapClient(uri);
+
+        try {
+            CoapResponse coapResponse = client.get();
+            if (coapResponse != null) {
+                response= coapResponse.getResponseText();
+            } else {
+                response= "No response received.";
+            }
+        } catch (ConnectorException | IOException e) {
+            response= "Got an error: " + e;
+        }
+
+        client.shutdown();
+        return response;
+
+    }
+
+}
--- app/src/main/java/com/example/coapplugin/HelloWorld.java
+++ app/src/main/java/com/example/coapplugin/HelloWorld.java
@@ -5,3 +5,5 @@ public class HelloWorld {
         return "Hello world!";
     }
 }
+
+
